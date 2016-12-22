41056ce8eec9a25d4e03c60fa341975fc57c30c1
matt <mtrower42@yahoo.com>
2016-12-22 15:33:12 -0800

    comments

--- scripts/incrementBot.js
+++ scripts/incrementBot.js
@@ -8,15 +8,17 @@ module.exports = function(robot) {
     	};
     	var lolNo = ['http://reactiongifs.com/?p=25434','http://reactiongifs.com/?p=24785','http://reactiongifs.com/?p=24469',
     				'http://reactiongifs.com/?p=24194','http://reactiongifs.com/?p=22145','http://reactiongifs.com/?p=21937' ]
-    	if (recipient === '@' + sender) {
+    	//prevent people ++ themselves
+        if (recipient === '@' + sender) {
     		return res.send(lolNo[random(lolNo)])
-    	} else {
+    	} else { //if the recip is new set to 1, otherwise add 1
 	    	if (robot.brain.data.users[recipient] === undefined) {
 	    		robot.brain.data.users[recipient] = 1
 	    	} else {
 	    		robot.brain.data.users[recipient] = robot.brain.data.users[recipient]+1
 	    	}
     	}
+        //send a random compliment with a ++
     	robot.http('https://spreadsheets.google.com/feeds/list/1eEa2ra2yHBXVZ_ctH4J15tFSGEu-VTSunsrvaCAV598/od6/public/values?alt=json')		
     		.get()(function(err, resp, body){ 
     			var list = JSON.parse(body).feed.entry;
@@ -33,13 +35,13 @@ module.exports = function(robot) {
     	var recipient = res.message.text.split(' ')[0];
     	var sender = res.message.user.name;
     	
-
+        //if the recip is new set to -1, otherwise subtract 1
     	if (robot.brain.data.users[recipient] === undefined) {
     		robot.brain.data.users[recipient] = -1
     	} else {
     		robot.brain.data.users[recipient] = robot.brain.data.users[recipient]-1
     	}
-    	if (recipient === '@' + sender) {
+    	if (recipient === '@' + sender) { // -- yourself gets a gif
     		res.send('http://www.reactiongifs.com/r/but-why.gif')
     	} else {
     		res.send(recipient + ', ' + '@' + sender + ' has unspeakable things to say about you, your family, and your way of life! \n Suffice it to say, you just lost a point :(')
@@ -48,29 +50,32 @@ module.exports = function(robot) {
     robot.hear(/(scoreboard)/i, function(res) {
     	var scoreBoard = [];
     	var people = robot.brain.data.users;
-
+        //transform brain obj into associative arrays
     	for (var points in people) {
-    		scoreBoard.push([points, people[points]])
+    		scoreBoard.push([points, people[points]]);
     	}
-
+        //sort by point count
     	var sorted = scoreBoard.sort(function(a,b) {
     	 return b[1] - a[1]
-    	}).map(function(x) {
-    		return x.join(' = ')
-    	}).join('\n');
-    	/*sorted[0][2] = ':crown:';
-    	console.log(sorted[0][2], sorted[0], sorted)
-    	robot.http('https://slack.com/api/emoji.list/xoxb-118260580103-fOctuK0xKky1X5sLdhZEqbcv')
-    		.get()(function(err, resp, body){
-    			console.log(resp)
-    		})*/
+    	});
+        //add emojis
+    	sorted[0].push(':crown:');
+    	sorted[1].push(':trophy:');
+    	sorted[2].push(':medal:');
+    	if (sorted[sorted.length-1][1] < 0) sorted[sorted.length-1].push(':smiling_imp:');
+        //format scoreboard output
+    	sorted = sorted.map(function(scoreArr) {
+    		return scoreArr.join(' = ')
+    	}).map(function(scoreStr) {
+    		return scoreStr = scoreStr.replace(/(= :)/g, '\t:')
+    	}).join('\n')
 
-    	res.send('The current standings are :\n' + sorted)
-    	
+    	res.send('The current standings are :\n' + sorted)	
     })
     robot.hear(/(nuke it [+]{2})/, function(res) {
+        var sender = res.message.user.name;
     	robot.brain.data.users = {};
-    	res.send('The standings have been reset')
+    	res.send('The standings have been reset by ' + sender)
     })
 };
 
